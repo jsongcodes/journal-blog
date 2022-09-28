908d83c6446b04ff7f446f234a2ae9d1700a962c
Jasmin <ssongnari@gmail.com>
2022-09-28 15:15:12 -0400

    Added useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,37 @@
+import { useState } from "react";
+
 const Home = () => {
-    return (
-        <div className="home">
-            <h2>Homepage</h2>
+  const [entries, setEntries] = useState([
+    {
+      title: "My new website",
+      body: "lorem ipsum...",
+      author: "Jasmin",
+      id: 1,
+    },
+    {
+      title: "My new website2",
+      body: "lorem ipsum...",
+      author: "Jasmin",
+      id: 2,
+    },
+    {
+      title: "My new website3",
+      body: "lorem ipsum...",
+      author: "Jasmin",
+      id: 3,
+    },
+  ]);
+
+  return (
+    <div className="home">
+      {entries.map((entry) => (
+        <div className="entry-preview" key={entry.id}>
+          <h2>{entry.title}</h2>
+          <p>Written by {entry.author}</p>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default Home;
