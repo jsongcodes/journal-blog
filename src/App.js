ee7e8951f891b4497941fbf780e78e0b014c8e6b
Jasmin <ssongnari@gmail.com>
2022-09-29 10:48:00 -0400

    Added Route to entry

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import EntryDetails from './EntryDetails';
 
 const App = () => {
   return (
@@ -15,6 +16,9 @@ const App = () => {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/entries/:id">
+              <EntryDetails />
             </Route>
           </Switch>
         </div>
