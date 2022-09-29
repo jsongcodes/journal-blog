6a00947fea5475baf035724078136ff36893913e
Jasmin <ssongnari@gmail.com>
2022-09-29 11:53:06 -0400

    Created fetch request

--- src/EntryDetails.js
+++ src/EntryDetails.js
@@ -1,11 +1,28 @@
-import {useParams} from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const EntryDetails = () => {
-    return(
-        <div className="entry-details">
-        <h2>Entry Details</h2>
-        </div>
-    )
-}
+  const { id } = useParams();
+  const [entries, setEntries] = useState(null);
+
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/entries/${id}`)
+    .then(resp => resp.json())
+    .then(data => setEntries(data))
+}, [])
+
+  return (
+    <div className="entry-details">
+        {entries && (
+            <article>
+                <h2>{entries.title}</h2>
+                <p>Written by {entries.author}</p>
+                <div>{entries.body}</div>
+            </article>
+        )}
+    </div>
+  );
+};
 
 export default EntryDetails;
--- src/EntryList.js
+++ src/EntryList.js
@@ -1,11 +1,14 @@
-const EntryList = ({ entries, title }) => {
+import { Link } from "react-router-dom";
+
+const EntryList = ({ entries }) => {
   return (
     <div className="entry-list">
-      <h2>{title}</h2>
       {entries.map((entry) => (
         <div className="entry-preview" key={entry.id}>
-          <h2>{entry.title}</h2>
-          <p>Written by {entry.author}</p>
+          <Link to={`/entries/${entry.id}`}>
+            <h2>{entry.title}</h2>
+            <p>Written by {entry.author}</p>
+          </Link>
         </div>
       ))}
     </div>
