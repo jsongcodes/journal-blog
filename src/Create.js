adc96f94c9ffc7f93549b954098ca8c437ff96cb
Jasmin <ssongnari@gmail.com>
2022-09-29 11:53:35 -0400

    Added controlled form

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,13 @@
 const Create = () => {
-    return (
-        <div>
-            <h2>Add a New Entry</h2>
-        </div>
-    )
-}
+  return (
+    <div className="create">
+      <h2>Add a New Entry</h2>
+      <form>
+        <label>Entry title:</label>
+        <input type="text" required />
+      </form>
+    </div>
+  );
+};
 
 export default Create;
