4b942c5bb6f9634adecb62d4506afa908cf84760
Jasmin <ssongnari@gmail.com>
2022-09-29 12:02:08 -0400

    Added to the controlled form and made css edits

--- src/Create.js
+++ src/Create.js
@@ -5,6 +5,15 @@ const Create = () => {
       <form>
         <label>Entry title:</label>
         <input type="text" required />
+        <label>Entry body:</label>
+        <textarea
+        required></textarea>
+        <label>Entry author:</label>
+        <select>
+            <option value="Jasmin">Jasmin</option>
+            <option value="New User">New User</option>
+        </select>
+        <button>Add Entry</button>
       </form>
     </div>
   );
