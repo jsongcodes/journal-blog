8ae6938886e901fd8b3c75e63849627d60a43169
Jasmin <ssongnari@gmail.com>
2022-09-29 10:47:35 -0400

    Added EntryDetails component

---
+++ src/EntryDetails.js
@@ -0,0 +1,11 @@
+import {useParams} from 'react-router-dom';
+
+const EntryDetails = () => {
+    return(
+        <div className="entry-details">
+        <h2>Entry Details</h2>
+        </div>
+    )
+}
+
+export default EntryDetails;
