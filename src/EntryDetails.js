fa3255d95aaf04b3ebd83387c092a3f2163aaf4f
Jasmin <ssongnari@gmail.com>
2022-09-29 13:58:22 -0400

    Added delete request

--- src/EntryDetails.js
+++ src/EntryDetails.js
@@ -1,26 +1,35 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
 const EntryDetails = () => {
   const { id } = useParams();
   const [entries, setEntries] = useState(null);
+  const history = useHistory();
 
+  const handleClick = () => {
+    fetch(`http://localhost:8000/entries/${id}`, {
+        method: 'DELETE',
+    }).then(() => {
+      history.push("/");
+    });
+  };
 
   useEffect(() => {
     fetch(`http://localhost:8000/entries/${id}`)
-    .then(resp => resp.json())
-    .then(data => setEntries(data))
-}, [])
+      .then((resp) => resp.json())
+      .then((data) => setEntries(data));
+  }, []);
 
   return (
     <div className="entry-details">
-        {entries && (
-            <article>
-                <h2>{entries.title}</h2>
-                <p>Written by {entries.author}</p>
-                <div>{entries.body}</div>
-            </article>
-        )}
+      {entries && (
+        <article>
+          <h2>{entries.title}</h2>
+          <p>Written by {entries.author}</p>
+          <div>{entries.body}</div>
+          <button onClick={handleClick}>delete</button>
+        </article>
+      )}
     </div>
   );
 };
