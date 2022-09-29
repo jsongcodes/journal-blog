027fda9394e396fcbe528526a4577eaa2b32aad7
Jasmin <ssongnari@gmail.com>
2022-09-29 13:48:49 -0400

    Added POST request

--- src/Create.js
+++ src/Create.js
@@ -1,17 +1,43 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Jasmin");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const entry = { title, body, author };
+    fetch('http://localhost:8000/entries', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(entry)
+    }).then(() => {
+        
+    })
+  };
+
   return (
     <div className="create">
       <h2>Add a New Entry</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Entry title:</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label>Entry body:</label>
         <textarea
-        required></textarea>
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
         <label>Entry author:</label>
-        <select>
-            <option value="Jasmin">Jasmin</option>
-            <option value="New User">New User</option>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Jasmin">Jasmin</option>
+          <option value="New User">New User</option>
         </select>
         <button>Add Entry</button>
       </form>
