2a4779bceb6aba4eb21cf05e51488a21b8208312
Jasmin <ssongnari@gmail.com>
2022-09-30 07:31:41 -0400

    Added journal prompt to the controlled form

--- src/Create.js
+++ src/Create.js
@@ -23,8 +23,14 @@ const Create = () => {
       <form onSubmit={handleSubmit}>
         <label>Entry prompt:</label>
         <select value={prompt} onChange={(e) => setPrompt(e.target.value)}>
-          <option value="Who is">Who is</option>
-          <option value="What is">What is</option>
+          <option value="What made you feel grateful today?">What made you feel grateful today?</option>
+          <option value="What is your favorite thing about yourself and why?">What is your favorite thing about yourself and why?</option>
+          <option value="Who inspires you? Why?">Who inspires you? Why?</option>
+          <option value="WWhere would you like to visit? Why?">Where would you like to visit? Why?</option>
+          <option value="What are your priorities today?">What are your priorities today?</option>
+          <option value="Looking back, what are some things you have achieved?">Looking back, what are some things you have achieved?</option>
+          <option value="Name 3 things you will do for yourself today.">Name 3 things you will do for yourself today.</option>
+          <option value="What makes you happy? Why?">What makes you happy? Why?</option>
         </select>
 
         <label>Entry body:</label>
@@ -34,10 +40,12 @@ const Create = () => {
           onChange={(e) => setBody(e.target.value)}
         ></textarea>
         <label>Entry date:</label>
-        <select value={date} onChange={(e) => setDate(e.target.value)}>
-          <option value="Jasmin">Jasmin</option>
-          <option value="New User">New User</option>
-        </select>
+        <input
+            type="date"
+            value={date}
+            placeholder="DD/MM/YY"
+            onChange={(e) => setDate(e.target.value)}
+          />
         <button>Add Entry</button>
       </form>
     </div>
